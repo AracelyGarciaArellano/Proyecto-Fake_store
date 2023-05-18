e371de58baeb0ad981dd527ee5360d06a58cd664
AracelyGarciaArellano <aracely.garcia193@tectijuana.edu.mx>
2023-05-18 01:23:53 -0700

    inicio y registro

--- Screens/InicioSesion.js
+++ Screens/InicioSesion.js
@@ -1,29 +1,123 @@
-import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { auth } from '../Firebase';
 
 const InicioSesion = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Validar el email y la contraseña y realizar la acción de inicio de sesión
+    if (!email || !password) {
+      return alert('Datos incorrectos');
+    }
+    
+    // Iniciar sesión con email y contraseña
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('Inicio de sesión exitoso');
+        // Redirigir al usuario a la pantalla de "Nosotros"
+        navigation.navigate('Nosotros');
+      })
+      .catch((error) => {
+        // Manejar el error
+        alert(error.message);
+      });
+  };
+
   return (
-    <View styles={styles.container}>
-      <Text></Text>
-      <Button
-        title="Home:Nosotros"
-        onPress={() => navigation.navigate('Nosotros')}
-      />
-      <Button
-        title="Registrate"
-        onPress={() => navigation.navigate('Registro')}
-      />
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          marginVertical: 18,
+          marginHorizontal: 16,
+          paddingHorizontal: 8,
+        }}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+        />
+
+        <TouchableOpacity onPress={handleLogin}>
+          <View
+            style={{
+              backgroundColor: '#2196F3',
+              width: 100,
+              height: 30,
+              borderRadius: 20,
+              alignSelf: 'center',
+              marginTop: 5,
+            }}>
+            <Text
+              style={{
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'white',
+                margin: 4,
+              }}>
+              Login
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <View
+            style={{
+              backgroundColor: 'gray',
+              width: 100,
+              height: 30,
+              borderRadius: 20,
+              alignSelf: 'center',
+              marginTop: 5,
+            }}>
+            <Text
+              style={{
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'white',
+                margin: 4,
+              }}>
+              Register
+            </Text>
+          </View> 
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default InicioSesion
+export default InicioSesion;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+  },
+  input: {
+    height: 40,
+    borderBottomWidth: 2,
+    borderTopWidth: 1,
+    margin: 5,
+    borderRadius: 20,
+    padding: 16,
   },
 });
--- Screens/Registro.js
+++ Screens/Registro.js
@@ -1,25 +1,98 @@
-import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
+import { Overlay } from '@rneui/themed';
+import { auth, db } from '../Firebase';
 
 const Registro = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  const handleRegister = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Registro exitoso, puedes realizar acciones adicionales aquí
+        updateUI(userCredential.user);
+      })
+      .catch((error) => {
+        // Si el registro falla, muestra un mensaje de error
+        setError('Authentication failed.');
+        updateUI(null);
+      });
+  };
+
+  const updateUI = (user) => {
+    if (user) {
+      setVisible(true);
+      // El usuario se registró correctamente, puedes realizar acciones adicionales aquí
+      // Ejemplo: redirigir al usuario a una pantalla de inicio
+      navigation.navigate('Home');
+    } else {
+      // El registro falló, puedes realizar acciones adicionales aquí
+    }
+  };
+
   return (
-    <View styles={styles.container}>
-      <Text></Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+        secureTextEntry={true}
+      />
+      <Button
+        title="Register"
+        onPress={handleRegister}
+      />
+
+      <Overlay isVisible={visible} onBackdropPress={() => setVisible(false)}>
+        <Text>Registro exitoso!!</Text>
+        <Text>Porfavor inicia sesion ahora</Text>
+        <Button
+          title="Start Building"
+          onPress={() => navigation.navigate('InicioSesion')} 
+        />
+      </Overlay>
+
       <Button
         title="Inicia Sesion"
-        onPress={() => navigation.goBack()}
+        onPress={() => navigation.navigate('InicioSesion')}
       />
+
+      {error && <Text>{error}</Text>}
     </View>
   );
 };
 
-export default Registro
+export default Registro;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    margin: 10,
+    paddingHorizontal: 10,
   },
 });
